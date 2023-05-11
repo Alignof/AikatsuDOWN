307db14ad9bdd2419869dde6c8992c6b2313b28d
Alingof <Alignof@outlook.com>
2023-05-11 23:59:54 +0900

    [add] implement downloading and saving to file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,30 @@
 use std::fs::File;
 
-fn main() -> Result<(), std::io::Error> {
-    let path = "data/aikatsup20230422.csv";
-    let file = File::open(path)?;
+#[derive(serde::Deserialize)]
+struct CsvData {
+    id: u32,
+    _words: String,
+    iamge_url: String,
+    _twitter_url: String,
+    _tags: Vec<String>,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let save_path = "target/images/";
+    let csv_path = "data/aikatsup20230422.csv";
+    let file = File::open(csv_path)?;
     let mut reader = csv::ReaderBuilder::new().flexible(true).from_reader(file);
 
     for record in reader.records() {
-        println!("{:?}", record?);
+        let csv_data: CsvData = record?.deserialize(None)?;
+        let url = csv_data.iamge_url;
+        let id = csv_data.id;
+        let image_bytes = reqwest::get(url).await?.bytes().await?;
+        let mut saving_file = File::create(format!("{save_path}{id}.jpg"))?;
+        std::io::copy(&mut image_bytes.as_ref(), &mut saving_file)?;
+
+        tokio::time::sleep(tokio::time::Duration::from_millis(1000)).await;
     }
 
     Ok(())
