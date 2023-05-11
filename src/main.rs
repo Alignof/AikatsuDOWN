82635be544b55c56d3c31c192833cb560f3e2a96
Alingof <Alignof@outlook.com>
2023-05-11 23:24:25 +0900

    [add] implement parsing csv

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+
+fn main() -> Result<(), std::io::Error> {
+    let path = "data/aikatsup20230422.csv";
+    let file = File::open(path)?;
+    let mut reader = csv::ReaderBuilder::new().flexible(true).from_reader(file);
+
+    for record in reader.records() {
+        println!("{:?}", record?);
+    }
+
+    Ok(())
 }
